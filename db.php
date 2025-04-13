bc461cf50e186ba27f9e6c02a95236b555af2021
ikalafe <imkalafe@gmail.com>
2025-04-13 09:09:14 -0400

    Refactor database connection variables and fix typo in error handling

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
 // Database configuration
-$servername = 'localhost';
-$username = 'kalafe';
-$password = 'danikalafe';
-$database = 'weblog';
+$db_servername = 'localhost';
+$db_username = 'kalafe';
+$db_password = 'danikalafe';
+$db_database = 'weblog';
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_database);
 
-if($conn->connect_err){
-    die("Connection failed: " . $conn->connect_err);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// echo "Connected successfully";
+// echo "Coxnnected successfully";
 ?>
